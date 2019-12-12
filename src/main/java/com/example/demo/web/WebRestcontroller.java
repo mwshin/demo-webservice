40b1ebc0a61d9ae940a32fec1e7eef7f22ce001c
mwshin <ai14@naver.com>
2019-12-12 17:22:43 +0900

    demo webservice 소스

---
+++ src/main/java/com/example/demo/web/WebRestcontroller.java
@@ -0,0 +1,13 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestcontroller {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "helloWorld";
+	}
+}
